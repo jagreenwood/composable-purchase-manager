452ba11241856b560d6858f8d4d7144b7fe3642f
Jeremy Greenwood <jagreenwood@gmail.com>
2021-01-26 19:48:03 -0500

    conform PurchaseError to Error

--- Sources/ComposablePurchaseManager/Models/PurchaseError.swift
+++ Sources/ComposablePurchaseManager/Models/PurchaseError.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum PurchaseError: Equatable {
+public enum PurchaseError: Error, Equatable {
     case cannotFindProduct
     case cannotMakePurchases
     case cannotFindReceipt
